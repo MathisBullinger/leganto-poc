6e472f0a9421b130845465d2c6cd7f61cc28360d
Mathis Bullinger <mathis@bullinger.dev>
2021-11-24 09:03:20 +0100

    section align

--- views/static/main.js
+++ views/static/main.js
@@ -1,19 +1,36 @@
 const panes = [...document.querySelectorAll('.split-view > div')]
-let lastPos = Array(panes.length).fill(0)
 
-panes.forEach((el) => el.addEventListener('scroll', onScroll))
+const stops = panes.map((v) => [...v.querySelectorAll('[data-align]')])
+for (const step of stops[0].map((_, i) => stops.map((v) => v[i]))) {
+  const max = Math.max(...step.map((v) => v.offsetTop))
+
+  for (const stop of step) {
+    const offset = stop.offsetTop
+    if (offset >= max) continue
+    const padd = document.createElement('div')
+    padd.style.height = `${max - offset}px`
+    padd.className = 'align'
+    stop.parentElement.insertBefore(padd, stop)
+  }
+}
+
+let lastPos = Array(panes.length).fill(0)
+const height = new Map(panes.map((v) => [v, v.scrollHeight - v.offsetHeight]))
+
+panes.forEach((el) => {
+  el.addEventListener('scroll', onScroll)
+})
 
 function onScroll({ currentTarget: el }) {
-  const other = panes[+(panes[0] === el)]
-
-  const top = Math.round(
-    (el.scrollTop / (el.scrollHeight - el.offsetHeight)) *
-      (other.scrollHeight - other.offsetHeight)
+  const pos = panes.map((v) =>
+    v === el
+      ? el.scrollTop
+      : Math.round((el.scrollTop / height.get(el)) * height.get(v))
   )
+  if (pos.every((v, i) => lastPos[i] === v)) return
+  lastPos = pos
 
-  const next = panes[0] === el ? [el.scrollTop, top] : [top, el.scrollTop]
-  if (next[0] === lastPos[0] && next[1] === lastPos[1]) return
-  lastPos = next
-
-  other.scrollTo({ top, behavior: 'auto' })
+  pos.forEach((top, i) => {
+    if (panes[i] !== el) panes[i].scrollTo({ top, behavior: 'auto' })
+  })
 }
